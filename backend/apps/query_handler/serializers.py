ec668d320a53fe3d404a396e0c857c1af0417025
Rishav <academicdahal21@gmail.com>
2025-06-21 10:05:49 +0545

    Refactor: query preprocessing pipline  Clean, tokenize, and lemmatize before saving as refined query

--- backend/apps/query_handler/serializers.py
+++ backend/apps/query_handler/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Query
+from apps.nlp_engine.tokenization import tokenize_text
 
 class QuerySerializer(serializers.ModelSerializer):
 
@@ -16,7 +17,7 @@ class QuerySerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         raw_query = validated_data.get('raw_query')
 
-        refined_query = None # pipeline_process(raw_query)
+        refined_query = tokenize_text(raw_query) # pipeline_process(raw_query)
 
         return Query.objects.create(
             raw_query=raw_query,
