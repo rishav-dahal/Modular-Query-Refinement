b70c72717aa132996d1e8b8d93c5c8a700aeaf06
sandhya <sandhya.201637@ncit.edu.np>
2025-06-21 09:03:56 +0545

    feat: update app name in QueryHandlerConfig and restore URL patterns for query submission

--- backend/apps/query_handler/apps.py
+++ backend/apps/query_handler/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class QueryHandlerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'query_handler'
+    name = 'apps.query_handler'
